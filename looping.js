7dc0751355d0e6d98a8c2f6cba64108fb80ed5c1
Anasomeir <anas.jawad.omair@gmail.com>
2021-03-07 14:37:39 +0200

    LABLOOPS

---
+++ looping.js
@@ -0,0 +1,29 @@
+var house = prompt("Do you want to buy a Villa or a Flat?");
+
+while(house != 'Villa' && house !='Flat')
+{
+     house = prompt ("Do you want to buy a Villa or a Flat?");
+}
+
+var housetype = "";
+
+if(house == "Villa")
+{
+   housetype = '<img src="villa.jpg" />';
+}
+
+else if(house == "Flat")
+
+{
+    housetype = '<img src="flat.png" />';
+}
+var numofRooms = prompt ("How many rooms do you want?");
+
+var result = '';
+
+for(var i = 0;i < numofRooms; i++)
+{
+    result = result + housetype;
+}
+
+document.write(result);
